7bd20f4eeeaee48f2547659b5bc863f37058a239
master-aira <sparamveer1001@gmail.com>
2023-05-28 11:46:26 +0530

    New

---
+++ HealthyProgrammer.py
@@ -0,0 +1,6 @@
+'''
+9AM - 5PM
+Water - water.mp3 (3.5 L or 250ml every 30min) - Drank - log
+Eyes - eyes.mp3 - every 30 min - EyDone - log
+Physical Activity - physical.mp3 every - 45 min -ExDone - log
+'''

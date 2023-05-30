8c0b91f9900292c50839364439b655998ad64d7c
master-aira <sparamveer1001@gmail.com>
2023-05-30 13:38:08 +0530

    30

---
+++ Abstraction_Encapsulation.py
@@ -0,0 +1,2 @@
+#Abstraction - Kuch kaam ya specific Tasks or Group of Tasks ko kehte hai
+#Encapsulation - Jaise koi function bs function apan ko detail me nhi jana hai , woh function ka andar sb en-capsule-lated hai
